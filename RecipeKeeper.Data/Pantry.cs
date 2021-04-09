69e706d1e3c81e30fb925405a49b1d0687a63fcd
Ashley Sharpe <ashleysharpe00@gmail.com>
2021-04-09 09:34:57 -0500

    Adds Data Layer for Pantry

--- RecipeKeeper.Data/Pantry.cs
+++ RecipeKeeper.Data/Pantry.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,16 @@ namespace RecipeKeeper.Data
 {
     public class Pantry
     {
+        [ForeignKey(nameof(Ingredient))]
+        public int IngredientId { get; set; }
+        public virtual Ingredient Ingredient { get; set; }
+        public string IngredientName { get; set; }
+        [Required]
+        public bool InStock { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public int Quantity { get; set; }
     }
 }
