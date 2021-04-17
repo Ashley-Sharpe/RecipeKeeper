db58c041a74fe4441bc3e151ec5b2dc47e9fa82a
Ashley Sharpe <ashleysharpe00@gmail.com>
2021-04-17 17:05:24 -0500

    Adds BookListItem and adds link to books on index

--- RecipeKeeper.Models/BookListItem.cs
+++ RecipeKeeper.Models/BookListItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,8 @@ namespace RecipeKeeper.Models
 {
     public class BookListItem
     {
-        //public int BookId { get; set; }
+        public int BookId { get; set; }
+        [Display(Name ="Book Title")]
         public string BookName { get; set; }
         public string Author { get; set; }
 
